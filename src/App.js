685c1c981712f8101422a6b5b2714715006e8f94
Stephrod <stephanie_rodriguez5@yahoo.com>
2016-11-21 22:50:50 -0600

    initial commit, wiring up app.js for react app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,45 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
+    const Contacts = [
+      {
+        _id: 'dale',
+        name: 'Dale Cooper',
+        occupation: 'FBI Agent',
+        avatar:
+      },
+      {
+        _id: 'spike',
+        name: 'Spike Speigel',
+        occupation: 'Bounty Hunter',
+        avatar:
+      },
+      {
+        _id: 'wirt',
+        name: 'Wirt',
+        occupation: 'Adventurer,'
+        avatar:
+      },
+      {
+        _id: 'michael',
+        name: 'Michael Myers',
+        occupation: 'Loving little brother',
+        avatar:
+      },
+      {
+        _id: 'dana',
+        name: 'Dana Scully',
+        occupation: 'FBI Agent',
+        avatar:
+      }
+    ];
     return (
       <div className="App">
-        <h1>Contact List!</h1>
+        <ul className="contact-list">
+        {contacts.map(contact => <li key={contact._id}>{contact.name}</li>}
+        </ul>
       </div>
-    );
+    );  
   }
 }
 
