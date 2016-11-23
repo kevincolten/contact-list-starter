b0119b8fee773e2196daa83c0623c946a4adf010
Stephrod <stephanie_rodriguez5@yahoo.com>
2016-11-23 16:08:09 -0600

    added ContactList, Contact, and SearchBar components to app

--- src/App.js
+++ src/App.js
@@ -1,46 +1,15 @@
 import React, { Component } from 'react';
+import ContactList from './ContactList';
+import SearchBar from './SearchBar';
 
 class App extends Component {
   render() {
-    const Contacts = [
-      {
-        _id: 'dale',
-        name: 'Dale Cooper',
-        occupation: 'FBI Agent',
-        avatar:
-      },
-      {
-        _id: 'spike',
-        name: 'Spike Speigel',
-        occupation: 'Bounty Hunter',
-        avatar:
-      },
-      {
-        _id: 'wirt',
-        name: 'Wirt',
-        occupation: 'Adventurer,'
-        avatar:
-      },
-      {
-        _id: 'michael',
-        name: 'Michael Myers',
-        occupation: 'Loving little brother',
-        avatar:
-      },
-      {
-        _id: 'dana',
-        name: 'Dana Scully',
-        occupation: 'FBI Agent',
-        avatar:
-      }
-    ];
     return (
       <div className="App">
-        <ul className="contact-list">
-        {contacts.map(contact => <li key={contact._id}>{contact.name}</li>}
-        </ul>
+        <SearchBar />
+        <ContactList />
       </div>
-    );  
+    );
   }
 }
 
